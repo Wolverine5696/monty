2b4fd3439e05c6b1f2e4928333aa99e3fe7e862c
Wolverine5696 <gomichigan5696@gmail.com>
2022-04-17 16:54:31 -0700

    adding comments

--- monty.h
+++ monty.h
@@ -8,10 +8,13 @@
 #include <unistd.h>
 
 /**
- * struct stack_s - doubly linked list representation of a stack
- * @n: int
- * @prev: points to the prev element of the stack
- * @next: points to the next element of the stack
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
  */
 
 typedef struct stack_s
@@ -22,10 +25,12 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struct instuction_s - opcode and its function
+ * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
- * 
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
  */
 
 typedef struct instruction_s
