72347ec5fc0276eb87b74c61bbf9fe3cf9793f9f
Wolverine5696 <gomichigan5696@gmail.com>
2022-04-17 16:13:55 -0700

    adding comments

--- push.c
+++ push.c
@@ -5,7 +5,8 @@
  *
  * @stack: pointer to the list's head
  * @line_number: line number read to determine order
- *
+ * @_errors: functions that check for errors
+ * @_parsearg: checks if second arg is a integer
  */
 
 void push(stack_t **stack, unsigned int line_number)
